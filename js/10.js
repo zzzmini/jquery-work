571e9114579710d157ce415ea643365aedff8dcd
zzzmini <sm01079200887@gmail.com>
2025-02-13 10:42:39 +0900

    forLoop

--- js/10.js
+++ js/10.js
@@ -7,4 +7,50 @@ $(document).ready(function(){
     { name : '이서', age : 18},
     { name : '가을', age : 23},
   ]
+
+  $("#btnforLoop").on("click", function(){
+    let result = ''
+    // for 구문
+    for(let i = 0; i < data.length; i++){
+      result = result + `
+        <tr>
+          <td>${i + 1}</td>
+          <td>${data[i].name}</td>
+          <td>${data[i].age}</td>
+        </tr>
+      `
+    }
+    $("#forOut").html(result)
+  })
+
+  $("#btnforEach").on("click", function(){
+    let result = '';
+    // for-Each, map 구문
+    data.map((x, index)=>{
+      result = result + `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${x.name}</td>
+          <td>${x.age}</td>
+        </tr>
+      `
+    })
+    $("#forEach").html(result)
+  })
+
+  $("#btnJQueryEach").on("click", function(){
+    let result = '';
+    
+    $.each(data, (index, x)=>{
+      result = result + `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${x.name}</td>
+          <td>${x.age}</td>
+        </tr>
+      `
+    })
+
+    $("#jqueryEach").html(result)
+  })
 })
